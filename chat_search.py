06308c7e9ea1eea6ba31845d74c9531ed1663a20
Sechaba Mokobane <37085213+stmoks@users.noreply.github.com>
2022-01-20 09:23:36 +0200

    Update chat_search.py

--- chat_search.py
+++ chat_search.py
@@ -1,7 +1,7 @@
 # the following will allow me to search through the text questions to ask specific questions similar to Google?
 # e.g. 
 # how many times do we speak about money?
-#  how many times do we speak about people? 
+# how many times do we speak about people? 
 # who initiates conversation? 
 # what is the tone between 2020 and 2021? 
 # what is the gender + age of the people speaking?
